c7fab651b4ddb5fbc5c19b46706157808ad9ba35
krivi <krivi95@hotmail.com>
2020-05-18 00:16:44 +0200

    Reading and cleaning Publications data

--- src/authors_data_processing.py
+++ src/authors_data_processing.py
@@ -2,10 +2,6 @@ import pandas as pd
 import os
 import math
 
-# Input file names
-AUTORS_FILE_NAME = 'UB_cs_authors.xlsx'
-PUBLICATIONS_FILE_NAME = 'UB_cs_papers_scopus'
-
 # Sheet names
 FACULTY_NAMES = [
     'matematicki fakultet',
@@ -21,6 +17,10 @@ AUTHORS_DEPARTMENT_NAME = 'Odsek'
 AUTHORS_FACULTY_NAME = 'Fakultet'
 
 class Author():
+    """
+    Author class containing all necessary attributes for nodes in scientific collaboration network graph.
+    Provides some additional methods for cleaning up dataset.
+    """
     autoincrement = 1
 
     def __init__(self, first_name, last_name, middle_name, department, faculty):
@@ -33,25 +33,26 @@ class Author():
         self.faculty = faculty
         self.number_of_publications = 0
         self.collaborators = []
+        self.articles = []
 
     def clean_data(self):
-        self.validate_middle_name()
-        self.change_all_names_to_lower_case()
-        self.replace_latin_characters()
+        self._validate_middle_name()
+        self._change_all_names_to_lower_case()
+        self._replace_latin_characters()
 
-    def validate_middle_name(self):
+    def _validate_middle_name(self):
         if isinstance(self.middle_name, float) and math.isnan(self.middle_name):
             self.middle_name = None 
 
-    def change_all_names_to_lower_case(self):
-         self.first_name = self.first_name.lower()
-         self.last_name = self.last_name.lower()
-         self.department = self.department.lower()
-         self.faculty = self.faculty.lower()
-         if self.middle_name:
-             self.middle_name = self.middle_name.lower()
+    def _change_all_names_to_lower_case(self):
+        self.first_name = self.first_name.lower()
+        self.last_name = self.last_name.lower()
+        self.department = self.department.lower()
+        self.faculty = self.faculty.lower()
+        if self.middle_name:
+            self.middle_name = self.middle_name.lower()
 
-    def replace_latin_characters(self):
+    def _replace_latin_characters(self):
         def replace_characters(name):
             name.replace('ć', 'c')
             name.replace('č', 'c')
@@ -80,23 +81,29 @@ class Author():
                 \n        department = {self.department} \
                 \n        faculty = {self.faculty}'
 
+class AuthorUtils():
+    """
+    Class containint various utility methods for dealing with authors as nodes of the scientific collaboration network graph. 
+    """
+    @staticmethod
+    def read_all_authors(path, file_name):
+        """Reads authors data from input file, does necessary data cleaning and returns dictionary of authors as result."""
+        authors = dict()
+        for faculty_name in FACULTY_NAMES:
+            data = pd.read_excel(os.path.join(path, file_name), sheet_name=faculty_name)
+            for column, row in data.iterrows():
+                author = Author(row[AUTHORS_FIRST_NAME], row[AUTHORS_LAST_NAME], row[AUTHORS_MIDDLE_NAME], row[AUTHORS_DEPARTMENT_NAME], row[AUTHORS_FACULTY_NAME])
+                author.clean_data()
+                authors[author.get_author_full_name_tuple]= author
+        return authors
 
-def read_all_authors():
-    """Reads authors data from input file, does necessary data cleaning and returns dictionary of authors as result."""
-    authors = dict()
-    for faculty_name in FACULTY_NAMES:
-        data = pd.read_excel(os.path.join('data', AUTORS_FILE_NAME), sheet_name=faculty_name)
-        for column, row in data.iterrows():
-            author = Author(row[AUTHORS_FIRST_NAME], row[AUTHORS_LAST_NAME], row[AUTHORS_MIDDLE_NAME], row[AUTHORS_DEPARTMENT_NAME], row[AUTHORS_FACULTY_NAME])
-            author.clean_data()
-            authors[author.get_author_full_name_tuple]= author
-    return authors
 
-# def clean_authors_data
-
+# Input file name
+AUTORS_FILE_NAME = 'UB_cs_authors.xlsx'
 
 if __name__ == '__main__':
-    all_authors = read_all_authors()
+
+    all_authors = AuthorUtils.read_all_authors('./dataset', AUTORS_FILE_NAME)
     # for full_author_name in all_authors:
     #     print(all_authors[full_author_name])
     print(len(all_authors))
